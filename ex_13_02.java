6d09268776e3edc4c2138049c01dced2ec263d84
Bekzhan17 <78722836+Bekzhan17@users.noreply.github.com>
2022-04-27 13:32:37 +0600

    Update ex_13_02.java

--- ex_13_02.java
+++ ex_13_02.java
@@ -2,36 +2,29 @@ package com.example.bekzhan;
 
 import java.util.ArrayList;
 
-public class Exercise_13_03 {
+public class Exercise_13_02 {
     public static void main(String[] args) {
         ArrayList<Number> list = new ArrayList<Number>();
-        list.add(14);
-        list.add(24);
-        list.add(4);
-        list.add(42);
-        list.add(5);
     
-        System.out.println("Unsorted: " + list.toString());
-        sort(list);
-        System.out.println("Sorted: " + list.toString());
+        list.add(21);
+        list.add(2);
+        list.add(24);
+        list.add(67);
+        list.add(1);
+        list.add(3);
+        list.add(53);
+
+        shuffle(list);
+
+        System.out.println(list.toString());
     }
 
-    public static void sort(ArrayList<Number> list) {
-        for (int i = 0; i < list.size(); i++) { 
-            Number currentMin = list.get(i); 
-            int currentMinIndex = i;
-
-            for (int j = i + 1; j < list.size(); j++) {
-                if (currentMin.doubleValue() > list.get(j).doubleValue()) {
-                    currentMin = list.get(j);
-                    currentMinIndex = j;
-                }
-            }   
-
-            if (currentMinIndex != i) {
-                list.set(currentMinIndex, list.get(i));
-                list.set(i, currentMin);
-            }
+    public static void shuffle(ArrayList<Number> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            int switchValue = (int) (Math.random() * list.size());
+            Number temp = list.get(switchValue);
+            list.set(switchValue, list.get(i));
+            list.set(i, temp);
         }
     }
 }
